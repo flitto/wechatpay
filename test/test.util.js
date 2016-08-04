8ddd9a9a8c639177aee895d6608eaa0259381c3a
Dan Kang <dann.kang@gmail.com>
2016-08-04 11:26:04 +0800

    test: add test cases

---
+++ test/test.util.js
@@ -0,0 +1,41 @@
+var should = require('should')
+  , util = require('../lib/util');
+
+describe('util test cases', function() {
+
+  it('generateNonceSting() with no parameter', function(done) {
+    var nonceStr = util.generateNonceString();
+    nonceStr.should.be.a.String();
+    nonceStr.length.should.be.equal(32);
+    done();
+  });
+
+  it('generateNonceSting(16)', function(done) {
+    var nonceStr = util.generateNonceString(16);
+    nonceStr.should.be.a.String();
+    nonceStr.length.should.be.equal(16);
+    done();
+  });
+
+  it('getXML()', function(done) {
+    var json = {return_code: 'SUCCESS', return_msg: 'OK'};
+
+    var xml = util.getXML(json);
+    xml.should.be.a.String();
+    done();
+  });
+
+  it('parserXML()', function(done) {
+    var xml = `<xml>
+        <return_code><![CDATA[SUCCESS]]></return_code>
+        <return_msg><![CDATA[OK]]></return_msg>
+      </xml>`;
+
+    util.parseXML(xml, function(err, data) {
+      should.not.exists(err);
+      data.should.be.an.Object();
+      data.should.have.properties(['return_code', 'return_msg']);
+      done();
+    });
+  });
+});
---
+++ test/test.wechatpay.js
@@ -0,0 +1,51 @@
+var should = require('should')
+  , WeChatPay = require('../lib/wechatpay').WeChatPay;
+
+describe('wechatpay test cases:', function() {
+
+  var wpay = new WeChatPay({appid: 'xxxxx', mch_id: 'xxxxx'});
+
+  it('call createUnifiedOrder() without initializing', function(done) {
+    var pay = new WeChatPay();
+    pay.createUnifiedOrder({}, function(err, data) {
+      should.exist(err);
+      done();
+    });
+  });
+
+  it('call createUnifiedOrder() with no parameter', function(done) {
+    wpay.createUnifiedOrder({}, function(err, data) {
+      should.exist(err);
+      done();
+    });
+  });
+
+  it('call queryOrder() with no parameter', function(done) {
+    wpay.queryOrder({}, function(err, data) {
+      should.exist(err);
+      done();
+    });
+  });
+
+  it('call closeOrder() with no parameter', function(done) {
+    wpay.closeOrder({}, function(err, data) {
+      should.exist(err);
+      done();
+    });
+  });
+
+  it('call refund() with no parameter', function(done) {
+    wpay.refund({}, function(err, data) {
+      should.exist(err);
+      done();
+    });
+  });
+
+  it('call transfer() with no parameter', function(done) {
+    wpay.transfer({}, function(err, data) {
+      should.exist(err);
+      done();
+    });
+  });
+
+});
